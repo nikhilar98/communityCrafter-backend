c4131ca7ffae6be5ec4ce14cdeb8e38bb1a15352
nikhilar98 <nikhilar98@gmail.com>
2023-11-17 13:02:13 +0530

    user register message update

--- app/middlewares/attachCertificateImages.js
+++ app/middlewares/attachCertificateImages.js
@@ -1,10 +1,10 @@
 const multer = require('multer')
 const upload = multer()
 const attachCertificateImages = (req,res,next)=>{
-    req.body.teachingCategories = JSON.parse(req.body.teachingCategories)  //only for postman testing. this will be sent as an obejct from frontend
+    req.body.teachingCategories = JSON.parse(req.body.teachingCategories)  
     const categoryFields = req.body.teachingCategories.map(ele=>({name:ele.categoryId}))  //for generating dyncamic fields array
     req.certificateFields = categoryFields 
-    upload.fields(req.certificateFields) //dynamic fields array passed - this middleware attached files object to the request object
+    upload.fields(req.certificateFields) //dynamic fields array passed - this middleware attaches files object to the request object
     console.log('checkpoint 0')
     next()
 }
