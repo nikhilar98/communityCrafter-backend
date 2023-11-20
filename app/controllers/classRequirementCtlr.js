9180a264af44a50265a150e92ecc00421a3b09ff
nikhilar98 <nikhilar98@gmail.com>
2023-11-20 12:10:51 +0530

    requirement ctlr populate creator

--- app/controllers/classRequirementCtlr.js
+++ app/controllers/classRequirementCtlr.js
@@ -56,7 +56,7 @@ classRequirementCtlr.getPendingrequirements = async (req,res) => {
             return res.status(400).json({errors:[{msg:"Please create your profile first to view the community requirements."}]})
         }
         const teacherCoordinates = teacherProfile.address.location.coordinates
-        const requirements = await ClassRequirement.find({status:"pending"}).populate('address')
+        const requirements = await ClassRequirement.find({status:"pending"}).populate('address').populate('creator')
         const filteredRequirements = requirements.filter(ele=>{
             return isPointWithinRadius(transformCoordinates(ele.address.location.coordinates),transformCoordinates(teacherCoordinates),15000)
                         //isPointWithinRadius({latitude:42.24222,longitude:12.32452},{latitude:20.24222,longitude:11.32452},radius in m )
